1a12b9a50319f228e6cd5965d53718895f2c6ef4
siddiqui3035 <s.h.siddiqui5830@gmail.com>
2022-03-10 21:51:07 +0600

    update process

---
+++ record.js
@@ -0,0 +1,36 @@
+
+    // create a async function
+    const start = async () => {
+
+        // navigator api add
+        const stream = await navigator.mediaDevices.getDisplayMedia(
+          {
+            // set a object. video stream
+            video: {
+              mediaSource: "screen",
+            },
+            audio: {
+              mediaSource:"screen",
+            }
+          });
+          const data=[];
+  
+        // this mediaRecorder can record our video and convert video to bynary object and store to the src.
+        const mediaRecorder= new MediaRecorder(stream);
+  
+        mediaRecorder.ondataavailable = (e) => {
+          data.push(e.data);
+        };
+  
+        mediaRecorder.start();
+  
+        mediaRecorder.onstop = (e) => {
+          document.querySelector("video").src = URL.createObjectURL(
+            new Blob(data,{
+              type: data[0].type,
+            })
+          );
+        };
+      };
+      // call the start function here
+      start();
